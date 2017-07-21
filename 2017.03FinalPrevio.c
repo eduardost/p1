576383b95edbbf03fe2d74007992d9e26cc9365a
eduardost <30332574+eduardost@users.noreply.github.com>
2017-07-21 00:44:05 -0300

    Create 2017.03FinalPrevio.c

---
+++ 2017.03FinalPrevio.c
@@ -0,0 +1,19 @@
+*/
+Programación I - Final Previo Marzo 2017
+
+En 1976 en la Universidad de Nottingham (Inglaterra) Graham Rawlinson realizó una serie de pruebas que demostraron que la alteración del orden de las letras de las palabras no afecta la capacidad de las personas de entender un texto. El científico conluyó que, para evitar problemas de lectura, basta conservar la primera y última letra de cada palabra. El siguiente párrafo ilustra las conclusiones del estudio:
+
+"Segeun un etsduio de una uivenrsdiad ignlsea, no ipmotra el odren en el qeu lsa ltears etsan ersciats, lo uicno ipormtnate es que  la pmrirea y la ultima ltera esten ecsritas en la psiocion cocrrtea. El rsteo peuden estar ttaolmntee mal y aun asi pordas lerelo sin pobrleams. Etso es pquore no lemeos cada ltera por si msima snio la paalbra cmoo un tdoo."
+
+Se solicita desarrollar un programa para aplicar esta técnica a un archivo de texto cualquiera, generando un archivo nuevo donde las palabras aparezcan con sus letras desordenadas según un patrón aleatorio, aunque la primera y la última letra deberán conservarse en su lugar (hasta el 50% del examen).
+
+Además de generar el nuevo archivo el programa deberá mostrar la lista de palabras del texto original ordenada de forma alfabética, y junto a cada palabra se indicará la cantidad de veces que se encontró en el texto. No hay un límite preestablecido para la cantidad de palabras (hasta el 50% del examen).
+
+Notas:
+1. El patrón de mezcla de las letras interiores de cada palabra debera ser completamente al azar.
+2. La longitud de las líneas y la posición de los signos de puntuación deben mantenerse ialteradas.
+3. Los números se conservarán intactos.
+4. El programa debe servir para cualquier archivo de texto.
+
+--------------
+/*
